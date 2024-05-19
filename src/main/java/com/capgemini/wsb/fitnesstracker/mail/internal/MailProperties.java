b092c74741ed7983a6ac56b9d163520031a10f2a
JurassicAlbert <albertjurasik@gmail.com>
2024-05-19 20:19:43 +0200

    modify mail properties

--- src/main/java/com/capgemini/wsb/fitnesstracker/mail/internal/MailProperties.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/mail/internal/MailProperties.java
@@ -12,11 +12,44 @@ import org.springframework.mail.javamail.JavaMailSender;
 @ConfigurationProperties(prefix = "mail")
 @Getter
 @RequiredArgsConstructor
-class MailProperties {
+public class MailProperties {
 
-    /**
-     * Email address that the email should be sent from.
-     */
-    private final String from;
+    private String host;
+    private int port;
+    private String username;
+    private String password;
 
+    // Getters and Setters
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
