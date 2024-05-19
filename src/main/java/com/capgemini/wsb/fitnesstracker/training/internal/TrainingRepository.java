c958313ded93d8033f67fe9838001f09e1748323
JurassicAlbert <albertjurasik@gmail.com>
2024-05-19 20:20:55 +0200

    set repository for checking user connected to training

--- src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingRepository.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingRepository.java
@@ -2,7 +2,11 @@ package com.capgemini.wsb.fitnesstracker.training.internal;
 
 import com.capgemini.wsb.fitnesstracker.training.api.Training;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-interface TrainingRepository extends JpaRepository<Training, Long> {
+public interface TrainingRepository extends JpaRepository<Training, Long> {
 
+    @Query("SELECT COUNT(t) > 0 FROM Training t WHERE t.user.id = :userId")
+    boolean existsByUserId(@Param("userId") Long userId);
 }
